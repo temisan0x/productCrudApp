9311d60e9e6e8ae191005d55d19f41f0ba0fae82
Temisan <73684278+heytemisan@users.noreply.github.com>
2021-03-05 11:24:44 +0100

    Update database.inc.php

--- database.inc.php
+++ database.inc.php
@@ -1,5 +1,5 @@
 <?php 
 
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'temisan', 'valentino');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'username', 'password');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
